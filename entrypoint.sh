858064f7e733b2d9fc8a18b6bd0b2c025a6828ae
Kyle N <kyle.ng7512@gmail.com>
2025-09-17 00:51:36 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,7 +32,8 @@ if [ ! -d "/server/AbioticFactor/Binaries/Win64" ] || [[ $AutoUpdate == "true" ]
 fi
 
 pushd /server/AbioticFactor/Binaries/Win64 > /dev/null
-exec WINEDLLOVERRIDES=$SetIsModded wine AbioticFactorServer-Win64-Shipping.exe $SetUsePerfThreads$SetNoAsyncLoadingThread-MaxServerPlayers=$MaxServerPlayers \
+export WINEDLLOVERRIDES=$SetIsModded
+exec wine AbioticFactorServer-Win64-Shipping.exe $SetUsePerfThreads$SetNoAsyncLoadingThread-MaxServerPlayers=$MaxServerPlayers \
     -PORT=$Port -QueryPort=$QueryPort -ServerPassword=$ServerPassword \
     -SteamServerName="$SteamServerName" -WorldSaveName="$WorldSaveName" -tcp $AdditionalArgs
 popd > /dev/null
