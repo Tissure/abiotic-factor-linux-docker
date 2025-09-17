ac993f32b5c13f731ea620ac5b5a21daf26f5eae
Kyle N <kyle.ng7512@gmail.com>
2025-09-17 00:19:34 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,6 +33,7 @@ fi
 
 pushd /server/AbioticFactor/Binaries/Win64 > /dev/null
 export WINEDLLOVERRIDES=$SetIsModded && \
+echo $WINEDLLOVERRIDES && \
 wine AbioticFactorServer-Win64-Shipping.exe $SetUsePerfThreads$SetNoAsyncLoadingThread-MaxServerPlayers=$MaxServerPlayers \
     -PORT=$Port -QueryPort=$QueryPort -ServerPassword=$ServerPassword \
     -SteamServerName="$SteamServerName" -WorldSaveName="$WorldSaveName" -tcp $AdditionalArgs
